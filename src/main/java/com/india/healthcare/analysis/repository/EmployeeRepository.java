f031489c4355fa2bf537e7a2c7320962ba11accf
Karthik Prabhu <kprabhu@na.rtdom.net>
2016-01-12 15:58:02 +0530

    commiting on git origin

--- src/main/java/com/india/healthcare/analysis/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.india.healthcare.analysis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.india.healthcare.analysis.domain.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-}
