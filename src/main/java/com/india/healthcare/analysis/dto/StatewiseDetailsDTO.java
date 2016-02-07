501d722d9e62e336468df6b9b0952ceabb1b7c2d
Karthik Prabhu <kprabhu@na.rtdom.net>
2016-02-07 16:08:19 +0530

    Removing stateName and stateCode from DTO

--- src/main/java/com/india/healthcare/analysis/dto/StatewiseDetailsDTO.java
+++ src/main/java/com/india/healthcare/analysis/dto/StatewiseDetailsDTO.java
@@ -4,26 +4,7 @@ package com.india.healthcare.analysis.dto;
  * This is the DTO containing statewise details of case and deaths due to various diseases.
  */
 public class StatewiseDetailsDTO {
-	
-	String stateCode;
-	String stateName;
 	CasesAndDeathsDTO details;
-	
-	public String getStateCode() {
-		return stateCode;
-	}
-	
-	public void setStateCode(String stateCode) {
-		this.stateCode = stateCode;
-	}
-	
-	public String getStateName() {
-		return stateName;
-	}
-	
-	public void setStateName(String stateName) {
-		this.stateName = stateName;
-	}
 	
 	public CasesAndDeathsDTO getDetails() {
 		return details;
@@ -31,11 +12,5 @@ public class StatewiseDetailsDTO {
 	
 	public void setDetails(CasesAndDeathsDTO details) {
 		this.details = details;
-	}
-	
-	@Override
-	public String toString() {
-		return "StatewiseDetailsDTO [stateCode=" + stateCode + ", stateName="
-				+ stateName + ", details=" + details + "]";
 	}
 }
